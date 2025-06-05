d47034a0242ef1bb7638dde286dd1b1aeadcded0
violexglajass <laungphoung.nang@my.jcu.edu.au>
2025-06-05 11:54:01 +0800

    check password

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,11 +1,12 @@
-password = '<PASSWORD>'
+PASSSWORD_LENGTH = 8
+
 def main():
     password = input ("Enter password:")
     print (is_valid_password(password))
 
 def is_valid_password (password):
     """Determine if password is considered valid."""
-    if len(password) < 6 or "" in password:
+    if len(password) < PASSWORD_LENGTH or "" in password:
         return False
     return True
 
