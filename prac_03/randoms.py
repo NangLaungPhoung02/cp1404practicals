a7f1cfccaa053dd135a84abe54e485e98da8b7df
violexglajass <laungphoung.nang@my.jcu.edu.au>
2025-06-05 11:53:29 +0800

    random number format

---
+++ prac_03/randoms.py
@@ -0,0 +1,11 @@
+import random
+random_int= random.randint(5, 20)
+print(random.randint(5, 20))
+
+
+random_range = (random.randrange)
+print(random.randrange(3, 10, 2))
+
+
+random_float = (random.uniform)
+print(random.uniform(2.5, 5.5))
