0bcade8d54adf46ab24feca1d1275904d0256712
violexglajass <laungphoung.nang@my.jcu.edu.au>
2025-06-19 17:26:23 +0800

    add code :3 to print all keys

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,7 +7,7 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+
 
 state_code = input("Enter short state: ").strip().upper()
 while state_code != "":
@@ -16,3 +16,8 @@ while state_code != "":
     except KeyError:
         print ("Invalid Short state")
     state_code = input("Enter short state: ").strip().upper()
+
+print ("\n All states and names:")
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
+
