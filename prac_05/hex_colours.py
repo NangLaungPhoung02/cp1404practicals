d53cd01f7ecf897d5a99d8e9aa554da3581caf85
violexglajass <laungphoung.nang@my.jcu.edu.au>
2025-06-19 17:58:17 +0800

    Add hex colour name, input colour name and validate error of input

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,6 +2,22 @@
 Estimate : 15 minutes
 Actual:  """
 
-Hex_colour_to_code = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff", "Alizarin crimson": "#e32636", "Amaranth": "#e52b50",
-              "Amber":"#ffbf00", "Amethyst": "#9966cc", "AntiqueWhite": "faebd7", "Aqua":"#00ffff", "Army Green": "#4b5320"}
-colour_name = input("Enter hex colour name:").upper()
+COLOUR_TO_HEX = {"Absolute Zero": "#0048ba",
+                 "Acid Green": "#b0bf1a",
+                 "Alice Blue": "#f0f8ff",
+                 "Alizarin crimson": "#e32636",
+                 "Amaranth": "#e52b50",
+              "Amber":"#ffbf00",
+                 "Amethyst": "#9966cc",
+                 "AntiqueWhite": "faebd7",
+                 "Aqua":"#00ffff",
+                 "Army Green": "#4b5320"}
+
+colour_name = input("Enter colour name:").strip().title()
+while colour_name !="":
+    try:
+        print (f"{colour_name} colour code is {COLOUR_TO_HEX[colour_name]}")
+    except KeyError:
+        print ("Invalid colour name")
+    colour_name = input("Enter colour name:").strip().title()
+
