fadbad66cf8d3cbe20c499756c2037e14d21030a
violexglajass <laungphoung.nang@my.jcu.edu.au>
2025-06-19 17:45:24 +0800

    input color name

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,5 +2,6 @@
 Estimate : 15 minutes
 Actual:  """
 
-Hex_colour = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff", "Alizarin crimson": "#e32636", "Amaranth": "#e52b50",
+Hex_colour_to_code = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff", "Alizarin crimson": "#e32636", "Amaranth": "#e52b50",
               "Amber":"#ffbf00", "Amethyst": "#9966cc", "AntiqueWhite": "faebd7", "Aqua":"#00ffff", "Army Green": "#4b5320"}
+colour_name = input("Enter hex colour name:").upper()
