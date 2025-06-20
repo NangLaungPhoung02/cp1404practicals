78fcaf372fb74f4d4289d40e632fdf41a411a842
violexglajass <laungphoung.nang@my.jcu.edu.au>
2025-06-20 18:38:51 +0800

    ask user to get name and email, extract name from email, confirm name yes or no, store the name and email in dictionary and print all emails and names

---
+++ prac_05/emails.py
@@ -0,0 +1,35 @@
+""" word occurrences
+Estimate : 30 minutes
+Actual: 1 hour and 15 minutes """
+
+def main():
+    """Main function to collect emails and names and display them."""
+    email_to_name = {}
+    email = input ("Enter your email:")
+    while email != "":
+        guessed_name = extract_name_from_email (email)
+        confirmation = input (f"Is your name {guessed_name}? (y/n)").lower ()
+        if confirmation != "" and confirmation != "y":
+            name = input ("Enter your name:")
+        else:
+            name = guessed_name
+
+        email_to_name[email] = name
+        email = input ("Enter your email:")
+
+    print ()
+    for email, name in email_to_name.items():
+        print (f"{name} ({email})")
+
+
+
+def extract_name_from_email(email):
+    """Extract a guessed name from an email address."""
+    username = email.split("@")[0]
+    parts = username.split(".")
+    name = "".join(parts)
+    return name
+
+main()
+
+
