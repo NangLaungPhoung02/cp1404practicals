9bbd2d7ac62433c8c302c26c96b0dcb3706b7fd1
violexglajass <laungphoung.nang@my.jcu.edu.au>
2025-06-20 17:16:50 +0800

    counts how many times of word, sorts the word and print words and numbers

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,25 @@
+""" word occurrences
+Estimate : 20 minutes
+Actual: 25 minutes """
+
+text = input ("Enter the text:")
+words = text.split()
+word_counts = {}
+
+for word in words:
+    word_counts[word] = word_counts.get(word, 0) + 1
+
+# for word in words:
+#   word = word.lower()
+#   if word in word_counts:
+#       word_counts[word] += 1
+#   else:
+#       word_counts[word] = 1
+
+sorted_words = sorted(word_counts.keys())
+max_length = max(len(word) for word in sorted_words)
+
+for word in sorted_words:
+    print(f"{word:{max_length}}:{word_counts[word]}")
+
+
