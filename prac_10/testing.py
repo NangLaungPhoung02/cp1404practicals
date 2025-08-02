34edecc13e9aef3fd0886842f8425d95f311a16e
violexglajass <laungphoung.nang@my.jcu.edu.au>
2025-08-03 00:22:42 +0800

    add format_sentence with 3 doctests.

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -62,7 +62,12 @@ def run_tests():
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
-    car = Car(fuel=10)
+    car_with_fuel = Car(fuel = 10)
+    assert car._ordometer == 0, "Car did not set fuel to 10"
+
+    car_default_fuel = Car()
+    assert car_default_fuel.fuel ==0, "Car did not default fuel to 0"
+
 
 
 run_tests()
